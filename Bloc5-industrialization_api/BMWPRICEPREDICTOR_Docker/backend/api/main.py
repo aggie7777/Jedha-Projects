97fab913b6ce8298b2e5b99bf9fd7eb935f56e54
Agnes <59807046+aggie7777@users.noreply.github.com>
2022-10-28 13:14:39 +0200

    Add files via upload
    Instant Machine Learning Web Application with Streamlit and FastAPI
    
    The goal of this project is to build complete end-to-end Regression ML model, Backend RestAPI to serve this model and front end UI so end user can interact and use it through web browser. Complete implementation is done Python language. archiBMW
    
    Build ML Model (In my case I did regression model)
    Develop REST API as Backend ( I use FastAPI framework in python)
    Develop Front End UI (I use Streamlit an open-source app framework as web applications)
    These two services can then be deployed in two Docker containers and orchestrated using Docker Compose. To wire the containers together I used docker-compose.yml Each service requires its own Dockerfile to assemble the Docker images. A Docker Compose YAML file is required to define and share both container applications.

---
+++ Bloc5-industrialization_api/BMWPRICEPREDICTOR_Docker/backend/api/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+import pickle
+
+# load model
+model=pickle.load(open("model\car_price_predictor_model.pkl",'rb'))
+
+app=FastAPI()
+# ["year", "mileage", "price", "fuelType", "transmission", "engineSize", "selling_price"]
+class Input(BaseModel):
+    year:int
+    mileage:int
+    price:int
+    fuelType:int
+    transmission:int
+    engineSize:float
+
+@app.get("/")
+def read_root():
+    return {"msg":"Car Price Predictor"}
+
+@app.post("/predict")
+def predict_price(input:Input):
+    data = input.dict()
+    data_in = [[data['year'], data['mileage'], data['price'], data['fuelType'],
+                    data['transmission'], data['engineSize']]]
+
+    prediction = model.predict(data_in)
+    return {
+        'prediction': prediction[0]
+        }
+
+if __name__=="__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
