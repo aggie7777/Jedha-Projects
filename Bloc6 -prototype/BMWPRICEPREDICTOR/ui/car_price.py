3d7d9a8030711197fb28a443bbcb355e91bbad39
Agnes <59807046+aggie7777@users.noreply.github.com>
2022-11-13 11:57:04 +0100

    Add files via upload

---
+++ Bloc6 -prototype/BMWPRICEPREDICTOR/ui/car_price.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import requests
+import json
+
+
+def main():
+    st.title("CAR PRICE PREDICTOR")
+
+    year = st.number_input("Year")
+
+    mileage = st.number_input("Mileage(in mile)")
+
+    price = st.number_input("Price(in £)")
+
+    fuelType = st.selectbox("fuelType", ("Diesel", "Electric", "Hybrid", "Petrol"))
+    if fuelType == "Diesel":
+        fuelType = 0
+    elif fuelType == "Electric":
+        fuelType = 1
+    elif fuelType == "Hybrid":
+        fuelType = 2
+    else:
+        fuelType = 3
+
+    transmission = st.selectbox("transmission", ("Automatic", "Semi-Auto", "Manual"))
+    if transmission == "Automatic":
+        transmission = 0
+    elif transmission == "Semi-Auto":
+        transmission = 1
+    else:
+        transmission = 2
+
+    engineSize = st.number_input("engineSize(in L)")
+
+
+    input_data = {
+        "year": year,
+        "mileage": mileage,
+        "price": price,
+        "fuelType": fuelType,
+        "transmission": transmission,
+        "engineSize": engineSize
+    }
+
+    price = 0
+    if st.button("Predict"):
+        price = requests.post(url="http://127.0.0.1:8000/predict", data=json.dumps(input_data))
+        price = price.json()
+        p = price['prediction']
+        st.success(f'The Price of the Car is {p} £')
+
+
+if __name__ == '__main__':
+    main()
